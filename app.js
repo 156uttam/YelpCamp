5a789bfcbf28645333c73802e11b132c675407ec
uttam singh <uttam907314@gmail.com>
2022-02-22 03:56:20 +0530

    added review

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const catchAsync=require('./utils/catchAsync');
 const ExpressError=require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review=require('./models/review');
+
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -82,6 +85,14 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}));
+app.post('/campgrounds/:id/reviews',catchAsync(async(req,res)=>{
+    const campground=await Campground.findById(req.params.id);
+    const review=new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect('/campgrounds/${campground._id}');
 }))
 app.all('*',(req,res,next)=>{
     next(new ExpressError('Page Not Found',404))
@@ -98,3 +109,8 @@ app.use((err,req,res,next)=>{
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 })
+
+
+   
+
+
