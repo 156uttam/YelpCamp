73f508c9549498a8264f13c2b1be03ea9638a598
uttam singh <uttam907314@gmail.com>
2022-03-03 15:10:19 +0530

    added authorisation

--- models/campground.js
+++ models/campground.js
@@ -1,27 +1,30 @@
 const mongoose = require('mongoose');
-const Review=require('./review')
+const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    image:String,
+    image: String,
     price: Number,
     description: String,
     location: String,
-    reviews:[
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
         {
-            type:Schema.Types.ObjectId,
-            ref:'Review'
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
         }
     ]
 });
 
-CampgroundSchema.post('findOneAndDelete',async function(doc){
-    if(doc)
-    {
-        await Review.remove({
-            _id:{
-                $in:doc.reviews
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
             }
         })
     }
--- seeds/index.js
+++ seeds/index.js
@@ -23,12 +23,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const price=Math.floor(Math.random()*20)+10;
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '5f5c330c2cd79d538f2c66d9',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image:'https://source.unsplash.com/collection/483251',
-            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet temporibus consequuntur minima dolorum accusantium deserunt placeat cumque, rem vero hic eaque, sit eveniet optio ullam doloribus iste, expedita consectetur tenetur?',
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price
         })
         await camp.save();
